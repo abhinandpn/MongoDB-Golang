7bf2a80cde660d7b11bc9880d351468b897d2ae7
abhinandpn <abhinandpn4067@gmail.com>
2025-02-24 11:27:47 +0530

    folder structure updating

--- config/config.go
+++ config/config.go
@@ -1,1 +1,17 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	fmt.Println("Environment variables loaded successfully")
+}
--- model/model.go
+++ model/model.go
@@ -1,5 +1,15 @@
 package model
 
-type User struct{
+import (
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name      string             `bson:"name" json:"name"`
+	Number    string             `bson:"number" json:"number"`
+	Email     string             `bson:"email" json:"email"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 }
