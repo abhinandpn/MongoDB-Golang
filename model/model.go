b4e21d121fdf0a4dab5a3d88b916e17f470b849d
abhinandpn <abhinandpn4067@gmail.com>
2025-02-25 10:26:58 +0530

    user model struct

--- model/model.go
+++ model/model.go
@@ -2,14 +2,12 @@ package model
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name      string             `bson:"name" json:"name"`
-	Number    string             `bson:"number" json:"number"`
-	Email     string             `bson:"email" json:"email"`
-	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UserID    string    `bson:"user_id" json:"user_id"` // Store UUID separately
+	Name      string    `bson:"name" json:"name"`
+	Number    string    `bson:"number" json:"number"`
+	Email     string    `bson:"email" json:"email"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 }
